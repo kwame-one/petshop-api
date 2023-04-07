97773c4ffa2ff869ec85fe6b2434ca0700832462
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-07 14:55:58 +0000

    add unique constraint to uuid column

--- database/migrations/2023_04_07_143158_create_categories_table.php
+++ database/migrations/2023_04_07_143158_create_categories_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->uuid();
+            $table->uuid()->unique();
             $table->string('title');
             $table->string('slug');
             $table->timestamps();
