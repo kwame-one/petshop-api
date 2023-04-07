8b7fb3d291ab2f5bc5da65043cc46d2b5c856b37
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-07 22:06:58 +0000

    update core controller

--- app/Http/Controllers/Api/CoreController.php
+++ app/Http/Controllers/Api/CoreController.php
@@ -6,8 +6,11 @@ use App\Http\Controllers\Controller;
 use App\Services\ICoreService;
 use App\Utils\AppUtil;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Pipeline\Pipeline;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 abstract class CoreController extends Controller
 {
@@ -36,6 +39,53 @@ abstract class CoreController extends Controller
                 ->through($this->filters())
                 ->thenReturn()
         );
+    }
+
+    /**
+     * store resource
+     *
+     * @param Request $request
+     * @return JsonResponse
+     * @throws ValidationException
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $validator = Validator::make(
+            $request->all(),
+            $this->service->model()::storeRules(),
+            $this->service->model()::errorMessages()
+        );
+
+        if ($validator->fails()) {
+            return response()->json(
+                AppUtil::response(null, null, $validator->errors()),
+                Response::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+
+        $data = $validator->validated();
+
+        return response()->json($this->service->store($data), Response::HTTP_CREATED);
+    }
+
+    /**
+     * update
+     *
+     * @param  $id
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function update($id, Request $request): JsonResponse
+    {
+        $data = $request->validate($this->service->model()::updateRules($id), $this->service->model()::errorMessages());
+
+        $resource = $this->service->update($id, $data);
+
+        if (!$resource) {
+            return response()->json(['message' => 'resource not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        return response()->json($resource);
     }
 
     /**
