1b0d0fc3ddc2bef6189d44febd2a557de1fd527b
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-07 14:54:59 +0000

    remove duplicate column called type from files migration

--- database/migrations/2023_04_07_140612_create_files_table.php
+++ database/migrations/2023_04_07_140612_create_files_table.php
@@ -17,7 +17,6 @@ return new class extends Migration {
             $table->string('path');
             $table->string('size');
             $table->string('type');
-            $table->string('type');
             $table->timestamps();
         });
     }
