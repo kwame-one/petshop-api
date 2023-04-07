12ffae3a23ee053a8837dc40b2582d8d0667fea6
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-07 23:09:04 +0000

    update paginate util

--- app/Utils/AppUtil.php
+++ app/Utils/AppUtil.php
@@ -13,10 +13,11 @@ class AppUtil
     public static function paginate($query)
     {
         $limit = request('limit') ?? 10;
-        $sortBy = request('sortBy') ?? 'created_at';
-        $orderBy = request()->boolean('desc') ? 'desc' : 'asc';
 
-        return $query->orderBy($sortBy, $orderBy)->paginate($limit);
+//        $sortBy = request('sortBy') ?? 'created_at';
+//        $orderBy = request()->boolean('desc') ? 'desc' : 'asc';
+
+        return $query->paginate($limit);
     }
 
     /**
