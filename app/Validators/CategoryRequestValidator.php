a7bdde55ee2d976db01a6c247c7d3f8a287ea597
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-08 01:12:58 +0000

    create category request validation rules

---
+++ app/Validators/CategoryRequestValidator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Validators;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
+
+trait CategoryRequestValidator
+{
+    public static function storeRules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'min:3'],
+        ];
+    }
+
+    public static function updateRules($id): array
+    {
+        return [
+            'title' => ['required', 'string', 'min:3'],
+        ];
+    }
+
+    public static function errorMessages(): array
+    {
+        return [];
+    }
+}
