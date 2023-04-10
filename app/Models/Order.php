123bac1a63495479b5c6850d9a7bf54eeace3239
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-10 21:44:44 +0000

    hide id field cast products and address field to json

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+use App\Validators\OrderRequestValidator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
     use HasFactory;
+    use OrderRequestValidator;
+
+    protected $guarded = ['id'];
+
+    protected $hidden = ['id'];
+
+    protected $casts = [
+        'products' => 'json',
+        'address' => 'json'
+    ];
 }
