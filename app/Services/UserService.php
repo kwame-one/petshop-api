a1ed445590e848e3c30f8332a50c2c37f285c20f
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-10 19:44:42 +0000

    inject password reset repository add function to create password reset token

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -4,16 +4,23 @@ namespace App\Services;
 
 use App\Repositories\ICoreRepository;
 use App\Repositories\JwtTokenRepository;
+use App\Repositories\PasswordResetRepository;
 use App\Utils\AppUtil;
+use Illuminate\Support\Str;
 
 class UserService extends CoreService
 {
     private JwtTokenRepository $jwtTokenRepository;
+    private PasswordResetRepository $passwordResetRepository;
 
-    public function __construct(ICoreRepository $repository, JwtTokenRepository $jwtTokenRepository)
-    {
+    public function __construct(
+        ICoreRepository $repository,
+        JwtTokenRepository $jwtTokenRepository,
+        PasswordResetRepository $passwordResetRepository
+    ) {
         parent::__construct($repository);
         $this->jwtTokenRepository = $jwtTokenRepository;
+        $this->passwordResetRepository = $passwordResetRepository;
     }
 
     /**
@@ -28,7 +35,7 @@ class UserService extends CoreService
 
         $jwtToken = [
             'user_id' => $user['id'],
-            'token_title' => 'Token generated for '.$user['uuid'],
+            'token_title' => 'Token generated for ' . $user['uuid'],
             'unique_id' => $token,
             'expires_at' => now()->addDays(30),
         ];
@@ -42,6 +49,13 @@ class UserService extends CoreService
     {
         $data['password'] = bcrypt($data['password']);
         return parent::update($id, $data);
+    }
+
+    public function sendPasswordResetToken(array $data): array
+    {
+        $data['token'] = Str::random(100);
+        $passwordReset = $this->passwordResetRepository->updateOrCreate($data);
+        return ['reset_token' => $passwordReset['token']];
     }
 
 }
