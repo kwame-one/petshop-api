a7adede3e3194d3bf36ea3908394197c4d3fabde
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-12 23:09:20 +0000

    clean category controller

--- src/app/Http/Controllers/Api/CategoryController.php
+++ src/app/Http/Controllers/Api/CategoryController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Filters\CreatedAt;
 use App\Filters\Order\CategoryOrder;
 
 class CategoryController extends CoreController
@@ -10,7 +9,6 @@ class CategoryController extends CoreController
     protected function filters(): array
     {
         return [
-            CreatedAt::class,
             CategoryOrder::class,
         ];
     }
