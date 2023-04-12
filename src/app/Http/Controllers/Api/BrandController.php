1e1d980ff44d8097298db265c7a45b7a2df07f4f
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-12 23:08:53 +0000

    clear brand controller

--- src/app/Http/Controllers/Api/BrandController.php
+++ src/app/Http/Controllers/Api/BrandController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Filters\CreatedAt;
 use App\Filters\Order\BrandOrder;
 
 class BrandController extends CoreController
@@ -10,7 +9,6 @@ class BrandController extends CoreController
     protected function filters(): array
     {
         return [
-            CreatedAt::class,
             BrandOrder::class,
         ];
     }
