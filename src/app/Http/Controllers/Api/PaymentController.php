7c4c6db88d64e00e6a0ee011c7222f40e9003416
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-12 23:10:41 +0000

    clean payment controller

--- src/app/Http/Controllers/Api/PaymentController.php
+++ src/app/Http/Controllers/Api/PaymentController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Filters\CreatedAt;
 use App\Filters\Order\PaymentOrder;
 
 class PaymentController extends CoreController
@@ -10,7 +9,6 @@ class PaymentController extends CoreController
     protected function filters(): array
     {
         return [
-            CreatedAt::class,
             PaymentOrder::class,
         ];
     }
