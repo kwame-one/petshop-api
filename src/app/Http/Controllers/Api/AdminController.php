97a3fc2bb72ce561ca57e189bc6bdc3fd7742676
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-14 09:06:52 +0000

    filter used in admin controller

--- src/app/Http/Controllers/Api/AdminController.php
+++ src/app/Http/Controllers/Api/AdminController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Filters\Address;
+use App\Filters\CreatedAt;
+use App\Filters\Marketing;
 use App\Filters\NonAdminUser;
 use App\Filters\Email;
 use App\Filters\FirstName;
@@ -14,12 +16,14 @@ class AdminController extends CoreController
     protected function filters(): array
     {
         return [
+            CreatedAt::class,
             NonAdminUser::class,
             FirstName::class,
             Phone::class,
             Address::class,
             Email::class,
             AdminOrder::class,
+            Marketing::class,
         ];
     }
 }
