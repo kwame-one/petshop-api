de883c95388fcdaa93ddd16a27c55176a33f48a7
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-15 19:12:59 +0000

    add test for creating category with insufficient permissions

--- src/tests/Feature/CategoryTest.php
+++ src/tests/Feature/CategoryTest.php
@@ -41,4 +41,17 @@ class CategoryTest extends TestCase
         $response->assertUnprocessable();
     }
 
+    public function test_create_category_without_permissions()
+    {
+        $user = User::factory(['is_admin' => 0])->create();
+        $loginResponse = $this->postJson("/api/v1/user/login", ['email' => $user->email, 'password' => 'password']
+        )->json();
+        $token = $loginResponse['data']['token'];
+
+        $categoryData = Category::factory()->make()->toArray();
+
+        $response = $this->withToken($token)->postJson('/api/v1/category/create', $categoryData);
+        $response->assertForbidden();
+    }
+
 }
