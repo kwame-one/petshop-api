87cf6cfc9ac6afff645ab46af715aa29375bd323
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-15 22:36:00 +0000

    call product seeder in database seeder

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             PostSeeder::class,
             PromotionSeeder::class,
             UserSeeder::class,
+            ProductSeeder::class,
         ]);
     }
 }
