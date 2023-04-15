393e435f555d7685d825fa4f57b3d60ba404771e
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-15 23:19:47 +0000

    create exchange rate converter controller for package

---
+++ packages/currency-exchange-rate/src/Http/Controllers/ExchangeRateConverterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kwame\CurrencyExchangeRate;
+
+use Illuminate\Http\Request;
+
+class ExchangeRateConverterController extends Controller
+{
+
+    public function convert(Request $request) {
+        
+    }
+}
