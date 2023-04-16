ce5044c22163e45005d2ed7b076ff1a392f0eeee
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-16 15:55:26 +0000

    resolve issue with endpoint

--- packages/currency-exchange-rate/routes/api.php
+++ packages/currency-exchange-rate/routes/api.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Kwame\CurrencyExchangeRate\Http\Controllers\ExchangeRateConverterController;
 
-Route::get('/currency-converter/{amount}/{currency}', [ExchangeRateConverterController::class, 'convert']);
+Route::get('/currency-converter/{currency}/{amount}', [ExchangeRateConverterController::class, 'convert']);
