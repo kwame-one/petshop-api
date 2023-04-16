7a234c632c2451efc7e177f6ae62b0b927c3b42c
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-16 16:59:23 +0000

    added check for providing invalid currency

--- packages/currency-exchange-rate/src/Http/Controllers/ExchangeRateConverterController.php
+++ packages/currency-exchange-rate/src/Http/Controllers/ExchangeRateConverterController.php
@@ -2,6 +2,7 @@
 
 namespace Kwame\CurrencyExchangeRate\Http\Controllers;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 use Kwame\CurrencyExchangeRate\Facades\ExchangeRateConverter;
 
 class ExchangeRateConverterController extends Controller
@@ -10,6 +11,9 @@ class ExchangeRateConverterController extends Controller
     public function convert($currency, $amount): JsonResponse
     {
         $data = ExchangeRateConverter::convert($amount, $currency);
+        if (!$data) {
+            return response()->json(['message' => 'currency not found'], Response::HTTP_BAD_REQUEST);
+        }
         return response()->json($data);
     }
 }
