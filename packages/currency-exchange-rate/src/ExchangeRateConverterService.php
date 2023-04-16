1821c3707fcd71c7b315001d0924ec7feaa323ce
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-16 15:54:18 +0000

    move fetching of rates to external class

--- packages/currency-exchange-rate/src/ExchangeRateConverterService.php
+++ packages/currency-exchange-rate/src/ExchangeRateConverterService.php
@@ -2,19 +2,33 @@
 
 namespace Kwame\CurrencyExchangeRate;
 
-use GuzzleHttp\Client;
+use Kwame\CurrencyExchangeRate\Services\RateService;
 
 class ExchangeRateConverterService
 {
-    private $client;
+    private RateService $rateService;
 
-    public function __construct()
+    public function __construct(RateService $rateService)
     {
-        $this->client  = new Client();
-
+        $this->rateService = $rateService;
     }
-    public function convert($amount, $currency): array {
-        return ['amount' => 10];
+
+    public function convert($amount, $currency)
+    {
+        $xml = simplexml_load_string(
+            $this->rateService->fetchRates('https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml')
+        );
+        $items = $xml->Cube->Cube->Cube;
+        $rates = [];
+        foreach ($items as $item) {
+            $curr = $item->attributes()->currency;
+            $rate = $item->attributes()->rate;
+            $rates[(string)$curr] = (string)$rate;
+        }
+        if (!isset($rates[$currency])) {
+            return false;
+        }
+        return ['amount' => number_format((float)$amount * (float)$rates[$currency], 2, '.', '')];
     }
 
 }
