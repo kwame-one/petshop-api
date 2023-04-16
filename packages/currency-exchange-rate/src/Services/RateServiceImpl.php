3c16d6578ade67c7619a1340e922e08d062064f4
Kwame Gyimah <kwameowusugyimah@gmail.com>
2023-04-16 15:51:49 +0000

    resolve bug in rate service impl

--- packages/currency-exchange-rate/src/Services/RateServiceImpl.php
+++ packages/currency-exchange-rate/src/Services/RateServiceImpl.php
@@ -7,6 +7,6 @@ class RateServiceImpl implements RateService
 
     public function fetchRates($url): bool|string
     {
-        return file_get_contents('https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml');
+        return file_get_contents($url);
     }
 }
